bbc74f23a1d1391a22694dcc5bae65c73488373b
Noori <nooriintikhab@gmail.com>
2019-04-18 09:07:06 -0400

    Updates

---
+++ precache-manifest.5a177a18ad21989934bea1dc87008b9b.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/resume/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e7e2d0b545ab3dd8967e",
+    "url": "/resume/static/css/main.6543b221.chunk.css"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "8637c9b176afd0f92a1bcd6a3b8b7412",
+    "url": "/resume/static/media/noori.8637c9b1.png"
+  },
+  {
+    "revision": "f831740dec65fa1ebe6d",
+    "url": "/resume/static/js/2.e7c6e948.chunk.js"
+  },
+  {
+    "revision": "8d8353aa1e34e4fcbebaae5d945d2ef3",
+    "url": "/resume/static/media/camp.8d8353aa.jpg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/resume/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/resume/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/resume/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/resume/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/resume/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/resume/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "e7e2d0b545ab3dd8967e",
+    "url": "/resume/static/js/main.a242477b.chunk.js"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/resume/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/resume/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/resume/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/resume/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/resume/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/resume/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/resume/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/resume/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/resume/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "f831740dec65fa1ebe6d",
+    "url": "/resume/static/css/2.95e44442.chunk.css"
+  },
+  {
+    "revision": "3de9d4392a6f3a3ace1e9efc7542e26c",
+    "url": "/resume/index.html"
+  }
+];
